15a3fd19ad1b4ebaf931576997dadfef4a3a337b
Luke <kamau.luke.m@gmail.com>
2024-05-03 21:33:37 +0300

    Add Form validation

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -44,7 +44,8 @@
                 return this.selectedCoach.description;
             },
             contactLink() {
-                return this.$route.path + '/' + this.id + '/contact';
+                // return this.$route.path + '/' + this.id + '/contact';
+                return this.$route.path + '/contact';
             }
         },
         created() {
